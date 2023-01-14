ec849ba457f9a1028e4ac912e84e196937eaf0de
Thomas de Visser <thomasdevisser@pm.me>
2023-01-14 18:57:31 +0100

    Finished the 26th lecture

---
+++ 26-code-structuring/src/Experience/Utils/Sizes.js
@@ -0,0 +1,19 @@
+import EventEmitter from "./EventEmitter.js";
+
+export default class Sizes extends EventEmitter {
+  constructor() {
+    super();
+
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+    // Resize event
+    window.addEventListener("resize", () => {
+      this.trigger("resize");
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+      this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+    });
+  }
+}
